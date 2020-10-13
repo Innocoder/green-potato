2902996e3c335ae98951f4332587f8ea0b00c5e3
Inno <innocentmonageng@gmail.com>
2020-10-13 18:55:47 +0200

    First commit

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+@import url('https://fonts.googleapis.com/css2?family=Arimo&family=Bitter&display=swap');
+
+body {
+    font-family:  sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    color: #333333;
+    line-height: 25px
+}
+
+.header-section {
+
+}
