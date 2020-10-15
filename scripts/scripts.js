48e915dffc6f1650e157f8d32c77afc41dfcd72e
Inno <innocentmonageng@gmail.com>
2020-10-15 15:15:39 +0200

    Second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,2 @@
-@import url('https://fonts.googleapis.com/css2?family=Arimo&family=Bitter&display=swap');
-
-body {
-    font-family:  sans-serif;
-    font-size: 14px;
-    font-weight: 400;
-    color: #333333;
-    line-height: 25px
-}
-
-.header-section {
-
-}
+document.getElementsByClassName("year").innerText = new Date().getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear();
