82b611fa5911abd2e056886df9f0e2df5896a833
Inno <innocentmonageng@gmail.com>
2020-10-17 09:56:50 +0200

    Home page mobile responsive

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,9 @@
 document.getElementsByClassName("year").innerText = new Date().getFullYear();
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+});
